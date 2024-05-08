83035b35df29634e447a8c4fcd3d358d9a707aa7
Cesarmontoya15 <142271553+Cesarmontoya15@users.noreply.github.com>
2024-05-07 23:06:04 -0500

    Añadir metodo nombre

--- assignment3/src/Main.java
+++ assignment3/src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
+    public static void nombre (){
+        System.out.println("Assignment 3 by Cesar Montoya Giraldo");
+    }
     public static void main(String[] args) {
+        nombre();
 
     }
 }
